6b50c31ff6cb90b632f7c3eb5e334c9096a83814
Dorzhi06 <dorzhi.gombozhapov@yandex.ru>
2021-11-15 04:37:14 +0900

    ChtotibSoft 0.21 - Correct fronted on homePage

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import chtotib from '../img/CHTOTiB.jpg';
+
 import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Расписание ЧТОТиБ</PanelHeader>
+		<PanelHeader>
+            <table>
+                <thead>
+                <tr>
+                    <th><h1>Расписание ИСиП-18-1</h1></th>
+                    <th><a href="https://www.chtotib.ru/"><img src={chtotib} width="45" height="45"/></a></th>
+                </tr>
+            </thead>
+            </table>
+        </PanelHeader>
 		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+		<Group header={<Header mode="secondary">Пользователь</Header>}>
 			<Cell
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
 				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
 			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				{`${fetchedUser.first_name} ${fetchedUser.last_name} группа ИСиП-18-1`}
 			</Cell>
 		</Group>}
 
-		<Group header={<Header mode="secondary">Navigation Example</Header>}>
+		<Group header={<Header mode="secondary">Навигация</Header>}>
             <Div>
                 <Button stretched size="l" mode="secondary" onClick={go} data-to="mainPage">
 					Расписание на понедельник
